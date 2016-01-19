146444559ecddbd32682bfc77806240b21a4ab40
Chris Tetreault <chris@doingmyprogramming.com>
2016-01-18 16:36:52 -0800

    I believe I have a reasonable Subst instance for TermMap

--- src/Types.hs
+++ src/Types.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Types where
 
 import qualified Data.Map.Strict as Map
+import qualified Data.List as List
 
-type VarId = String
+type VarId = Integer
 
 data Term a =
    App Int [Term a]
@@ -14,9 +16,11 @@ data Term a =
    | UVar a
    deriving (Eq, Ord)
 
+{-
 data RefSelf a =
    Cyc (RefSelf (Maybe (Term a)))
    deriving (Eq, Ord)
+-}
 
 data Rule a =
    Rule { vars :: VarId,
@@ -27,7 +31,15 @@ data Rule a =
 type TermMap a = Map.Map VarId (Term a)
 
 class Subst key subst where
-   fresh :: VarId -> subst -> ([key], subst)
+   fresh :: Integer -> subst -> ([key], subst)
    empty :: subst
    lookup :: subst -> key -> Maybe (Term key)
    insert :: key -> Term key -> subst -> Maybe subst
+
+instance Subst VarId ((TermMap VarId), Integer) where
+   fresh n (m, i) = ([i .. (i + n)], (m, i + n))
+   empty = (Map.empty, 0)
+   lookup (m, _) k = Map.lookup k m
+   insert k t (m, i) = if k `Map.member` m
+                       then Nothing
+                       else Just ((Map.insert k t m), i)
