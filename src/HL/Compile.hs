e14421640db218da04b22d39ac638d5299eecd27
Chris Tetreault <chris@doingmyprogramming.com>
2016-02-02 11:09:47 -0800

    some work done

--- src/HL/Compile.hs
+++ src/HL/Compile.hs
@@ -4,7 +4,7 @@ import Types
 import Tactic
 import HL
 import Control.Monad.State
-import qualified Data.Map.Strict as Map
+import qualified Data.Map as Map
 
 {-
    For now, implementing without memoization. Will revisit
@@ -19,14 +19,18 @@ compile prg = undefined
 
 
 
-compileHl :: (Subst k s)
-             => HL k Integer
-             -> State s (Maybe [Term k])
-compileHl hl = undefined
+compileHl :: HL k Integer
+             -> State (SubstEnv VarId) (Maybe [Term k])
+compileHl (HLCall name) = do
+   env <- get
+   let sub = lkup env name
+   undefined
 
-compileHc :: (Subst k s)
-             => HC k Integer
-             -> State s (Maybe [Term k])
+compileHl HLFail = return Nothing
+
+
+compileHc :: HC k Integer
+             -> State (SubstEnv VarId) (Maybe [Term k])
 compileHc (HCAll hl) = compileHl hl
 compileHc (HCEach hls) = do
    res <- sequence $ map compileHl hls
