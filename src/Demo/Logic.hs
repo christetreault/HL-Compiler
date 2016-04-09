6c9ec49b1a9ff425046246eac59768e763a7c5ac
Chris Tetreault <chris@doingmyprogramming.com>
2016-04-09 13:49:04 -0700

    thoughts on potential solutions

--- src/Demo/Logic.hs
+++ src/Demo/Logic.hs
@@ -42,3 +42,16 @@ fairButBad = do
       do
          guard (even (length [0..]))
          return x
+
+{-
+A potential solution to this issue is to create some sort of "parallel mplus"
+pmplus :: MonadPlus m => m a -> m a -> m a that runs both the lhs and rhs
+in parrallel and combines results in the order that they are produced.
+
+However, I'm not sure if it's ok that for some monad a and b, a `mplus` b
+might return [1, 2], and a `pmplus` b might return [2, 1]. This doesn't
+seem to violate any laws, but may alter the meaning of our programs.
+
+If this isn't an issue, I think it could be done, and (observe fairButBad)
+would return the same result as (observe fair)
+-}
